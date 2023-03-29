e328012ce49094618867351b57473321a7f24f45
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2023-03-28 21:05:20 -0300

    feat; Corrigido

--- 3.laços de iteração/02-switch-case/switch-case.js
+++ 3.laços de iteração/02-switch-case/switch-case.js
@@ -1,14 +1,6 @@
 "use strict";
-/* eslint-disable prefer-const */
-/* eslint-disable prettier/prettier */
-/**
- * arquivo: switch-case.ts
- * descrição: arquivo responsável por ensinar como usar instrução switch-case no Typescript
- * data: 23/12/2021
- * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
- */
 Object.defineProperty(exports, "__esModule", { value: true });
-// ==> Exemplo 01 - Uso do switch...case
+//Exemplo 01 - Uso do switch...case
 let flor = 'tulipa';
 switch (flor) {
     case 'rosa':
@@ -23,7 +15,7 @@ switch (flor) {
     default:
         console.log('Por favor, selecione uma outra flor!');
 }
-// ==> Exemplo 02 - Uso do switch...case
+//Exemplo 02 - Uso do switch...case
 let diaUtilSemana = 5;
 switch (diaUtilSemana) {
     case 0:
--- 3.laços de iteração/02-switch-case/switch-case.ts
+++ 3.laços de iteração/02-switch-case/switch-case.ts
@@ -1,15 +1,6 @@
-/* eslint-disable prefer-const */
-/* eslint-disable prettier/prettier */
-/**
- * arquivo: switch-case.ts
- * descrição: arquivo responsável por ensinar como usar instrução switch-case no Typescript
- * data: 23/12/2021
- * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
- */
-
 export { };
 
-// ==> Exemplo 01 - Uso do switch...case
+//Exemplo 01 - Uso do switch...case
 
 let flor = 'tulipa';
 
@@ -28,7 +19,7 @@ switch (flor) {
     console.log('Por favor, selecione uma outra flor!');
 }
 
-// ==> Exemplo 02 - Uso do switch...case
+//Exemplo 02 - Uso do switch...case
 
 let diaUtilSemana = 5;
 
