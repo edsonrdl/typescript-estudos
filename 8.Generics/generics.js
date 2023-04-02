a4d23e4ffc9157aa69e6f3ee1c88b70edba47f0b
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2023-04-02 18:54:58 -0300

    feat: Criado Generics

---
+++ 8.Generics/generics.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//Exemplo 01: Generics
+function retornarElementosRandomicos(items) {
+    let itemRandomico = Math.floor(Math.random() * items.length);
+    return items[itemRandomico];
+}
+;
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let numerosRandomicos = retornarElementosRandomicos(numeros);
+console.log(numerosRandomicos);
+let estados = ['São Paulo', 'Rio de Janeiro', 'Minas Gerais', 'Paraná', 'Santa Catarina', 'Rio Grande do Sul'];
+let estadosRandomicos = retornarElementosRandomicos(estados);
+console.log(estadosRandomicos);
+// =Exemplo 02: Generics
+function exibirElementos(array) {
+    array.forEach((elemento) => {
+        console.log(elemento);
+    });
+}
+;
+let number = [1, 2, 3, 4, 5];
+let states = ['São Paulo', 'Rio de Janeiro', 'Minas Gerais', 'Paraná', 'Santa Catarina', 'Rio Grande do Sul'];
+exibirElementos(number);
+exibirElementos(states);
