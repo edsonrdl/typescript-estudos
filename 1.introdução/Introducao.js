e811b77e1208f8c40828b4fcc365060c3fb7942d
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2023-05-05 00:27:10 -0300

    feat: corrigido

---
+++ 1.introdução/Introducao.js
@@ -0,0 +1,4 @@
+"use strict";
+//Para fazer  a instalação do tsconfig.json e copilar as pastas ts,basta instalar na raiz do arquivo usando o terminal e digitando tsc --init
+let mensagem = 'Olá,Mundo';
+console.log(mensagem);
--- 1.introdução/Introducao.ts
+++ 1.introdução/Introducao.ts
@@ -1,4 +1,4 @@
 //Para fazer  a instalação do tsconfig.json e copilar as pastas ts,basta instalar na raiz do arquivo usando o terminal e digitando tsc --init
 
-let mensagem: string = 'ty4wetyertyertyerty';
+let mensagem: string = 'Olá,Mundo';
 console.log(mensagem);
