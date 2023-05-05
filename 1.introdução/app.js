1cbcf4f4328f31c792b2ac15d465f20c71b06bb1
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2023-05-05 00:09:15 -0300

    feat: corrigido erro

--- 1.introdução/app.js
+++ 1.introdução/app.js
@@ -1,4 +1,7 @@
 "use strict";
 //Para fazer  a instalação do tsconfig.json e copilar as pastas ts,basta instalar na raiz do arquivo usando o terminal e digitando tsc --init
-let mensagem = 'ty4wetyertyertyerty';
+//E para executar npm i -D sucrase
+//Depois só usar o terminal e usar  dessa forma como exemplo npx sucrase-node 1.introdução/app.ts
+//Ou pode usar a extensão code run
+let mensagem = 'olá,pessoal! ';
 console.log(mensagem);
